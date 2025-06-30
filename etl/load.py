efacde4c39b4cc9465d8657c7671bfb2515ad3a6
sophiasussa <sophiasussa@outlook.com>
2025-06-30 11:26:44 -0300

    feat: integração inicial com SQLite

--- etl/load.py
+++ etl/load.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 
-def to_database(df):
-    engine = create_engine("sqlite:///database/oscs.db")  # ou outro banco
-    df.to_sql("oscs", con=engine, if_exists="replace", index=False)
+def to_database(dataframe):
+    engine = create_engine('sqlite:///database/oscs.db')
+    
+    dataframe.to_sql('oscs', con=engine, if_exists='replace', index=False)
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from etl import extract, transform, load
 
 def main():
+    print("🔍 Iniciando extração...")
     raw_data = extract.from_csv("data/oscs.csv")
+    print("✅ Extração concluída.")
+
+    print("🧼 Iniciando transformação...")
     clean_data = transform.clean(raw_data)
+    print("✅ Transformação concluída.")
+
+    print("📦 Iniciando carga...")
     load.to_database(clean_data)
+    print("✅ Dados carregados com sucesso no SQLite.")
 
 if __name__ == "__main__":
     main()
